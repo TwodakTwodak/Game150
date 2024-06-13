a627a636591f0379d165e720df551b846419ad48
pokemon0819 <144295807+pokemon0819@users.noreply.github.com>
2024-06-13 17:33:28 +0900

    ds

--- Game150/Game/Wall.h
+++ Game150/Game/Wall.h
@@ -112,3 +112,37 @@ private:
 
 	Top top;
 };
+
+//class VerticFloor : public CS230::GameObject {
+//public:
+//	VerticFloor(Math::vec3 position);
+//	GameObjectTypes Type() override { return GameObjectTypes::FlatFloor; }
+//	std::string TypeName() override { return "VerticFloor"; }
+//	virtual void ResolveCollision(GameObject* other_object) override;
+//	bool CanCollideWith(GameObjectTypes);
+//private:
+//	CurrentDimension dimension;
+//	enum class Animations {
+//		Side,
+//		Top
+//	};
+//	class Side : public State {
+//	public:
+//		virtual void Enter(GameObject* object) override;
+//		virtual void Update(GameObject* object, double dt) override;
+//		virtual void CheckExit(GameObject* object) override;
+//		std::string GetName() override { return "VerticFloor Side"; }
+//	};
+//
+//	Side side;
+//
+//	class Top : public State {
+//	public:
+//		virtual void Enter(GameObject* object) override;
+//		virtual void Update(GameObject* object, double dt) override;
+//		virtual void CheckExit(GameObject* object) override;
+//		std::string GetName() override { return "VerticFloor Top"; }
+//	};
+//
+//	Top top;
+//};
